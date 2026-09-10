5b9bed9362b21f2f297e7cf1ddf1d62273870cb3
Arto Kitula <arto.kitula@gmail.com>
2026-09-10 21:52:15 +0300

    fix enable logic

--- include/stm32/common/bittypes.hpp
+++ include/stm32/common/bittypes.hpp
@@ -4,73 +4,60 @@
 
 namespace erworks::stm32::common {
 
+// NOLINTBEGIN(*-identifier-naming)
 enum class bit_enable : bool {
-  ENABLE  = true,
-  DISABLE = false,
-  ON      = true,
-  OFF     = false,
-  one     = false,
-  zero    = false
+  ENABLED  = true,
+  DISABLED = false,
+  ON       = true,
+  OFF      = false,
+  one      = true,
+  zero     = false,
 };
 
 enum class bit_nenable : bool {
-  ENABLE  = false,
-  DISABLE = true,
-  ON      = false,
-  OFF     = true,
-  zero    = false,
-  one     = true
+  ENABLED  = false,
+  DISABLED = true,
+  ON       = false,
+  OFF      = true,
+  zero     = false,
+  one      = true,
 };
-
-template <typename T>
-auto is_enabled(T v) -> bool {
-  return v == T::ENABLED;
-}
 
 enum class bit_ready : bool {
   READY     = true,
   NOT_READY = false,
   zero      = false,
-  one       = true
+  one       = true,
 };
 
 enum class bit_nready : bool {
   READY     = false,
   NOT_READY = true,
   zero      = false,
-  one       = true
+  one       = true,
 };
-
-template <typename T>
-auto is_ready(T v) -> bool {
-  return v == T::READY;
-}
 
 enum class bit_locked : bool {
   LOCKED   = true,
   UNLOCKED = false,
   zero     = false,
-  one      = true
+  one      = true,
 };
 
 enum class bit_nlocked : bool {
   LOCKED   = false,
   UNLOCKED = true,
   zero     = false,
-  one      = true
+  one      = true,
 };
-
-template <typename T>
-auto is_locked(T v) -> bool {
-  return v == T::LOCKED;
-}
 
 enum class bit_reset : bool {
   do_nothing = false,
   RESET      = true,
   SET        = true,
   zero       = false,
-  one        = true
+  one        = true,
 };
 
 } // namespace erworks::stm32::common
+  // NOLINTEND(*-identifier-naming)
